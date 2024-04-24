cc1149fdd09b3bb25386da092c61808718cafe41
HardikPrajapati98 <79240929+HardikPrajapati98@users.noreply.github.com>
2024-04-24 17:33:33 +0530

    screen

---
+++ src/screen/countryInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+const CountryInfo = () => {
+    return (
+        <Text>
+
+        </Text>
+    );
+};
+
+export default CountryInfo;
---
+++ src/screen/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+const Login = () => {
+    return (
+        <Text>
+            
+        </Text>
+    );
+};
+
+export default Login;
