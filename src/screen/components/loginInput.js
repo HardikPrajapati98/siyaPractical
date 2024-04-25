ec8d79d785a2c35dc523f126722475c9a70a300f
HardikPrajapati98 <79240929+HardikPrajapati98@users.noreply.github.com>
2024-04-25 19:31:19 +0530

    screen

--- src/screen/components/loginInput.js
+++ src/screen/components/loginInput.js
@@ -1,11 +1,11 @@
 import React, { forwardRef, useImperativeHandle, useRef, useState } from "react"
-import { Text, TextInput ,View} from 'react-native'
+import {Image, StyleSheet,Text, TextInput, View} from 'react-native'
 import { loginStyles } from "../../styleSheet/login";
 
 const LoginInputBox = forwardRef((props, ref) => {
     const [loginProp,setLoginProp] = useState({})
     const passowrdRef = useRef(null)
-    
+
     useImperativeHandle(ref, () => (({
         getProps:() =>loginProp
 
@@ -16,7 +16,7 @@ const LoginInputBox = forwardRef((props, ref) => {
         setLoginProp(preState => {
             return {...preState,[type]:e}
         })
-        
+
     }
 
     const updateSecureEntery = () => {
@@ -27,7 +27,7 @@ const LoginInputBox = forwardRef((props, ref) => {
 
 
     return <View style={loginStyles.inputBox}>
-        
+
         <Text style={loginStyles.headerText}>
             Log in To Scale
         </Text>
@@ -41,22 +41,32 @@ const LoginInputBox = forwardRef((props, ref) => {
                 onChange={(e) => updateState(e, 'email')}
                  placeholderTextColor={"#fff"}
 
-             
             />
             <Text style={loginStyles.inputText}> Password</Text>
-            <TextInput
-                placeholder="Enter Password"
-                ref={passowrdRef}
-                style={loginStyles.inputView}
-                value={loginProp?.password || ''}
-                onChange={(e) => updateState(e, 'password')}
-                secureTextEntry={true}
-                placeholderTextColor={"#fff"}
-            />
+            <View style={{flexDirection:'row'}}>
+                    <TextInput
+                        placeholder="Enter Password"
+                        ref={passowrdRef}
+                        style={loginStyles.inputView}
+                        value={loginProp?.password || ''}
+                        onChange={(e) => updateState(e, 'password')}
+                        secureTextEntry={true}
+                        placeholderTextColor={"#fff"}
+                    />
+                    <View style={{...StyleSheet.absoluteFillObject,right:0,height: 50, width: 25,backgroundColor:'red'}}>
+                        <Image
+                            source={{uri: 'https://cdn.iconscout.com/icon/free/png-256/free-eye-504-450305.png'}}
+                            style={{height: 25, width: 25}}
+                        />
+                    </View>
+
+
+            </View>
+
         </View>
 
-    
-             
+
+
     </View>
 
 })
