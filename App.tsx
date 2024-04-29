d6606aaa9f60f1ba9d0061957de44581c8ac87e4
HardikPrajapati98 <79240929+HardikPrajapati98@users.noreply.github.com>
2024-04-29 09:57:46 +0530

    screen

--- App.tsx
+++ App.tsx
@@ -16,6 +16,9 @@ import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
 import HomeScreen from "./src/index";
+import { Provider } from 'react-redux';
+import store from "./src/store/index";
+
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -24,33 +27,17 @@ function App(): React.JSX.Element {
     flex:1
   };
   return (
+    <Provider store={store}>
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <HomeScreen/>
-    </SafeAreaView>
+      </SafeAreaView>
+      </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
